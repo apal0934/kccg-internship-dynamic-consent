5c46d6d56a4fca45694dac029df8c0dc2ab4af89
Alex Palmer <alex@rivi.io>
2020-01-14 12:02:00 +1100

    Added: CORS Middleware

--- dynamicconsentprototype/main.py
+++ dynamicconsentprototype/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from graphene import Field, List, ObjectType, Schema, String
 from mongoengine import connect, disconnect_all
 from starlette.graphql import GraphQLApp
+from starlette.middleware.cors import CORSMiddleware
 
 from dynamicconsentprototype.models.consent import ConsentModel
 from dynamicconsentprototype.models.user import UserModel
@@ -42,4 +43,12 @@ def shutdown_db_client():
     disconnect_all()
 
 
+origins = ["http://localhost", "http://localhost:8000", "http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_route("/", GraphQLApp(schema=Schema(query=Query, mutation=Mutations)))
