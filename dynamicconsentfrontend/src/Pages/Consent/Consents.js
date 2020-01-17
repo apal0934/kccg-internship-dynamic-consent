cdc0b71052a971bb08899d2f08f80f8f418b739d
Alex Palmer <alex@rivi.io>
2020-01-17 14:35:54 +1100

    Added delete user and consent functionalities

--- dynamicconsentfrontend/src/Pages/Consent/Consents.js
+++ dynamicconsentfrontend/src/Pages/Consent/Consents.js
@@ -1,8 +1,10 @@
-import { Icon, Layout, Table } from "antd";
+import { Button, Icon, Layout, Table } from "antd";
+import React, { Component } from "react";
 
-import React from "react";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
 
 const { Content } = Layout;
 
@@ -17,42 +19,120 @@ const GET_CONSENTS = gql`
     }
 `;
 
-const columns = [
-    {
-        title: "Name",
-        dataIndex: "name",
-        key: "name"
-    },
-    {
-        title: "Purpose",
-        dataIndex: "purpose",
-        key: "purpose"
-    },
-    {
-        title: "Non-commercial",
-        dataIndex: "commercial",
-        key: "commercial",
-        render: record =>
-            !record ? <Icon type="check" /> : <Icon type="close" />
+export class Consents extends Component {
+    state = {
+        data: [],
+        loading: true,
+        client: []
+    };
+
+    componentDidMount() {
+        const cache = new InMemoryCache();
+        const link = new HttpLink({
+            uri: "http://localhost:8000"
+        });
+        const client = new ApolloClient({
+            cache,
+            link
+        });
+
+        client
+            .query({
+                query: GET_CONSENTS
+            })
+            .then(result =>
+                this.setState({
+                    data: result.data,
+                    client: client
+                })
+            );
     }
-];
 
-const Consents = () => {
-    const { data, loading } = useQuery(GET_CONSENTS);
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state !== prevState && this.state.loading) {
+            if (this.state.data.consents) {
+                this.setState({
+                    loading: false
+                });
+            }
+        }
+    }
 
-    return (
-        <Content style={{ padding: "0 50px" }}>
-            <div style={{ padding: 24, minHeight: 280 }}>
-                <Table
-                    columns={columns}
-                    loading={loading}
-                    dataSource={data ? data.consents : []}
-                    rowKey="id"
+    delete(e, record) {
+        e.preventDefault();
+        const consents = this.state.data.consents.filter(
+            consent => consent.id !== record.id
+        );
+        const data = { consents };
+        this.setState({
+            data: data
+        });
+        const DELETE_CONSENT = gql`
+        mutation {
+            deleteConsent(id: "${record.id}") {
+                consent {
+                    name
+                    purpose
+                    commercial
+                }
+            }
+        }`;
+        this.state.client.mutate({
+            mutation: DELETE_CONSENT
+        });
+    }
+
+    columns = [
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name"
+        },
+        {
+            title: "Purpose",
+            dataIndex: "purpose",
+            key: "purpose"
+        },
+        {
+            title: "Non-commercial",
+            dataIndex: "commercial",
+            key: "commercial",
+            render: record =>
+                !record ? <Icon type="check" /> : <Icon type="close" />
+        },
+        {
+            title: "",
+            key: "action",
+            render: record => (
+                <Button
+                    ghost
+                    icon="delete"
+                    type="danger"
+                    shape="round"
                     size="small"
+                    onClick={e => this.delete(e, record)}
                 />
-            </div>
-        </Content>
-    );
-};
+            )
+        }
+    ];
+
+    render() {
+        return (
+            <Content style={{ padding: "0 50px" }}>
+                <div style={{ padding: 24, minHeight: 280 }}>
+                    <Table
+                        columns={this.columns}
+                        loading={this.state.loading}
+                        dataSource={
+                            !this.state.loading ? this.state.data.consents : []
+                        }
+                        rowKey="id"
+                        size="small"
+                    />
+                </div>
+            </Content>
+        );
+    }
+}
 
 export default Consents;
--- dynamicconsentfrontend/src/Pages/User/Users.js
+++ dynamicconsentfrontend/src/Pages/User/Users.js
@@ -1,9 +1,11 @@
-import { Icon, Layout, Table } from "antd";
+import { Button, Icon, Layout, Table } from "antd";
+import React, { Component } from "react";
 
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import { Link } from "react-router-dom";
-import React from "react";
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
 
 const { Content } = Layout;
 
@@ -18,58 +20,134 @@ const GET_USERS = gql`
     }
 `;
 
-const columns = [
-    {
-        title: "Name",
+export class Users extends Component {
+    state = {
+        data: [],
+        client: [],
+        loading: true
+    };
 
-        key: "firstName",
-        render: record => (
-            <span>
-                {record.firstName} {record.lastName}
-            </span>
-        )
-    },
-    {
-        title: "Email",
-        dataIndex: "email",
-        key: "email"
-    },
-    {
-        title: "",
-        key: "action",
-        render: (text, record) => (
-            <span>
-                <Link
-                    to={{
-                        pathname: `/user/${record.id}`,
-                        state: {
-                            firstName: record.firstName
-                        }
-                    }}
-                >
-                    <Icon type="control" style={{ fontSize: 24 }} />
-                </Link>
-            </span>
-        )
+    componentDidMount() {
+        const cache = new InMemoryCache();
+        const link = new HttpLink({
+            uri: "http://localhost:8000"
+        });
+        const client = new ApolloClient({
+            cache,
+            link
+        });
+
+        client
+            .query({
+                query: GET_USERS
+            })
+            .then(result =>
+                this.setState({
+                    data: result.data,
+                    client: client
+                })
+            );
     }
-];
 
-const Users = () => {
-    const { data, loading } = useQuery(GET_USERS);
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state !== prevState && this.state.loading) {
+            if (this.state.data.users) {
+                this.setState({
+                    loading: false
+                });
+            }
+        }
+    }
 
-    return (
-        <Content style={{ padding: "0 50px" }}>
-            <div style={{ padding: 24, minHeight: 280 }}>
-                <Table
-                    columns={columns}
-                    loading={loading}
-                    dataSource={data ? data.users : []}
-                    rowKey="id"
+    delete(e, record) {
+        e.preventDefault();
+        const users = this.state.data.users.filter(
+            user => user.id !== record.id
+        );
+        const data = { users };
+        this.setState({
+            data: data
+        });
+        const DELETE_USER = gql`
+        mutation {
+            deleteUser(id: "${record.id}") {
+                user {
+                    firstName
+                    lastName
+                    email
+                }
+            }
+        }`;
+        this.state.client.mutate({ mutation: DELETE_USER });
+    }
+
+    columns = [
+        {
+            title: "Name",
+
+            key: "firstName",
+            render: record => (
+                <span>
+                    {record.firstName} {record.lastName}
+                </span>
+            )
+        },
+        {
+            title: "Email",
+            dataIndex: "email",
+            key: "email"
+        },
+        {
+            title: "",
+            key: "action",
+            render: (text, record) => (
+                <span>
+                    <Link
+                        to={{
+                            pathname: `/user/${record.id}`,
+                            state: {
+                                firstName: record.firstName
+                            }
+                        }}
+                    >
+                        <Icon type="control" style={{ fontSize: 24 }} />
+                    </Link>
+                </span>
+            )
+        },
+        {
+            title: "",
+            key: "action",
+            render: record => (
+                <Button
+                    ghost
+                    icon="delete"
+                    type="danger"
+                    shape="round"
                     size="small"
+                    onClick={e => this.delete(e, record)}
                 />
-            </div>
-        </Content>
-    );
-};
+            )
+        }
+    ];
+
+    render() {
+        return (
+            <Content style={{ padding: "0 50px" }}>
+                <div style={{ padding: 24, minHeight: 280 }}>
+                    <Table
+                        columns={this.columns}
+                        loading={this.state.loading}
+                        dataSource={
+                            !this.state.loading ? this.state.data.users : []
+                        }
+                        rowKey="id"
+                        size="small"
+                    />
+                </div>
+            </Content>
+        );
+    }
+}
 
 export default Users;
