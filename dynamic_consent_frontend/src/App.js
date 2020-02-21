42027d31742795561527dfc0e6a066521f8b174e
Alex Palmer <alexpalmer@Alexs-MacBook-Pro.local>
2020-02-21 15:31:41 +1100

    Add proof of concept disclaimer

--- dynamic_consent_frontend/src/App.js
+++ dynamic_consent_frontend/src/App.js
@@ -3,9 +3,12 @@ import { Route, HashRouter as Router, Switch } from "react-router-dom";
 import AddUser from "./Pages/User/AddUser";
 import Header from "./Components/Header";
 import Landing from "./Pages/Landing";
+import { Layout } from "antd";
 import React from "react";
 import User from "./Pages/User/User";
 import Users from "./Pages/User/Users";
+
+const { Footer } = Layout;
 
 function App() {
     const API_IP = "localhost";
@@ -13,21 +16,31 @@ function App() {
     return (
         <div className="App">
             <Router>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={Landing} />
-                    <Route
-                        exact
-                        path="/user/list"
-                        render={() => <Users IP={API_IP} />}
-                    />
-                    <Route
-                        exact
-                        path="/user/add"
-                        render={() => <AddUser IP={API_IP} />}
-                    />
-                    <Route path="/user/:id" component={User} />
-                </Switch>
+                <Layout style={{ backgroundColor: "white" }}>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={Landing} />
+                        <Route
+                            exact
+                            path="/user/list"
+                            render={() => <Users IP={API_IP} />}
+                        />
+                        <Route
+                            exact
+                            path="/user/add"
+                            render={() => <AddUser IP={API_IP} />}
+                        />
+                        <Route path="/user/:id" component={User} />
+                    </Switch>
+                    <Footer
+                        style={{
+                            textAlign: "center",
+                            backgroundColor: "white"
+                        }}
+                    >
+                        Proof of Concept Only
+                    </Footer>
+                </Layout>
             </Router>
         </div>
     );
